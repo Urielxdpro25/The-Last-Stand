0519a5ca1111eee600bed237c143510e8c3efe2d
Urielxdpro25 <urieluncos@gmail.com>
2025-10-24 11:01:52 -0300

    funciona el movimiento del barco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,10 +10,10 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
-  IonItem,
-  IonLabel,
-  IonSelect,
-  IonSelectOption,
+  // IonItem,
+  // IonLabel,
+  // IonSelect,
+  // IonSelectOption,
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -28,10 +28,10 @@ import { FormsModule } from '@angular/forms';
     IonToolbar,
     IonTitle,
     IonContent,
-    IonItem,
-    IonLabel,
-    IonSelect,
-    IonSelectOption,
+    // IonItem,
+    // IonLabel,
+    // IonSelect,
+    // IonSelectOption,
     CommonModule,
     FormsModule,
   ],
@@ -92,21 +92,30 @@ export class HomePage implements AfterViewInit {
     this.player.x = this.canvas.width / 2 - this.player.width / 2;
     this.player.y = this.canvas.height - this.player.height - 10;
   }
-
-  update() {
-    if (this.keys['ArrowLeft']) {
-      this.player.x -= this.speed;
-    }
-    if (this.keys['ArrowRight']) {
-      this.player.x += this.speed;
-    }
-
-    // Limitar movimiento a los bordes del canvas
-    if (this.player.x < 0) this.player.x = 0;
-    if (this.player.x + this.player.width > this.canvas.width) {
-      this.player.x = this.canvas.width - this.player.width;
-    }
+update() {
+  if (this.keys['ArrowLeft']) {
+    this.player.x -= this.speed;
   }
+  if (this.keys['ArrowRight']) {
+    this.player.x += this.speed;
+  }
+  if (this.keys['ArrowUp']) {
+    this.player.y -= this.speed;
+  }
+  if (this.keys['ArrowDown']) {
+    this.player.y += this.speed;
+  }
+
+  // Limitar movimiento a los bordes del canvas
+  if (this.player.x < 0) this.player.x = 0;
+  if (this.player.x + this.player.width > this.canvas.width) {
+    this.player.x = this.canvas.width - this.player.width;
+  }
+  if (this.player.y < 0) this.player.y = 0;
+  if (this.player.y + this.player.height > this.canvas.height) {
+    this.player.y = this.canvas.height - this.player.height;
+  }
+}
 
   draw() {
     // Fondo azul
